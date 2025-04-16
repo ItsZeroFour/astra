3e110e0c7ee736f9eb8373c396bd5acefe4150cd
itsZeroFour <itsZeroFourX@gmail.com>
2025-04-16 17:42:46 +0300

    add mobile

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./header.module.scss";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import { motion, AnimatePresence } from "framer-motion";
 import angleDown from "../../assets/icons/angle-down.svg";
+import angleDown2 from "../../assets/icons/angle-down-2.svg";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   const [isCatalogOpen, setIsCatalogOpen] = useState(false);
+  const [openMenuServices, setOpenMenuServices] = useState(false);
 
   const handleMouseEnter = () => {
     setIsCatalogOpen(true);
@@ -15,6 +18,17 @@ const Header = () => {
   const handleMouseLeave = () => {
     setIsCatalogOpen(false);
   };
+
+  useEffect(() => {
+    if (openMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [openMenu]);
 
   return (
     <header className={style.header}>
@@ -74,7 +88,58 @@ const Header = () => {
           </nav>
 
           <div />
+
+          <button
+            className={`${style.header__menu} ${openMenu ? style.active : ""}`}
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <div></div>
+          </button>
         </div>
+
+        {openMenu && (
+          <div className={style.header__menu__wrapper}>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">О КОМПАНИИ</Link>
+                </li>
+
+                <li>
+                  <button
+                    onClick={() => setOpenMenuServices(!openMenuServices)}
+                  >
+                    КАТАЛОГ УСЛУГ <img src={angleDown2} alt="angle" />
+                  </button>
+
+                  {openMenuServices && (
+                    <ul>
+                      <li>
+                        <Link to="/">Системы безопасности</Link>
+                      </li>
+
+                      <li>
+                        <Link to="/">Автоматика</Link>
+                      </li>
+
+                      <li>
+                        <Link to="/">Двери</Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+
+                <li>
+                  <Link to="/">ТАРИФЫ</Link>
+                </li>
+
+                <li>
+                  <Link to="/">КОНТАКТЫ</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
       </div>
     </header>
   );
