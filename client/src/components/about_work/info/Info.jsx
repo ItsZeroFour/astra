639fd8dcc8b8b125b8436af02027f7a2c4551038
itsZeroFour <itsZeroFourX@gmail.com>
2025-05-15 11:31:06 +0300

    fix

--- client/src/components/about_work/info/Info.jsx
+++ client/src/components/about_work/info/Info.jsx
@@ -3,6 +3,8 @@ import style from "./info.module.scss";
 import figure from "../../../assets/images/about/figure.svg";
 
 const Info = ({ work }) => {
+  console.log(work.text_about);
+
   return (
     <section className={style.info}>
       <div className="container">
@@ -10,7 +12,13 @@ const Info = ({ work }) => {
           <div className={style.info__left}>
             <h3>{work.title_about}</h3>
 
-            <p>{work.text_about}</p>
+            <p>
+              {work.text_about
+                .split(/(<br\s?\/?>)/i)
+                .map((part, index) =>
+                  part.toLowerCase().includes("<br") ? <br key={index} /> : part
+                )}
+            </p>
           </div>
 
           <div className={style.info__right}>
